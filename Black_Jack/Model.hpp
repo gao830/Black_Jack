8509b0c5f1fb41f8181ebfca9540850fa138b87e
CodyGilman <cgccb@mst.edu>
2017-04-09 15:08:41 -0500

    Model Class updates

--- Black_Jack/Model.hpp
+++ Black_Jack/Model.hpp
@@ -11,4 +11,20 @@
 
 #include <stdio.h>
 
+class Model {
+	public:
+	int deck[4][13];
+	int playerHand[10];
+	int dealerHand[10];
+	int playerHandSize = 0;
+	int dealerHandSize = 0;
+	private:
+	void initDeck();
+	void drawCard(int currentPlayer);
+	void determineWinner();
+
+};
+
+
+
 #endif /* Model_hpp */
