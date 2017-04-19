41ef7db1c90c8a70b824411b8394e824bfa2ee87
SeanLangford <spl5z6@mst.edu>
2017-04-19 15:07:16 -0500

    add new line statements for output readability

--- Black_Jack/View.cpp
+++ Black_Jack/View.cpp
@@ -7,16 +7,19 @@ View::View() {
 
 void View::greeting() {
     cout << "Welcome to Black Jack game!" << endl;
+    cout << endl;
 }
 
 void View::promptForHoldOrHit() {
-    cout << "Do you want to hold or hit? (1 for hold, 2 for hit)" << endl;    
+    cout << "Do you want to hold or hit? (1 for hold, 2 for hit)" << endl;
+    cout << endl;
     return;
 }
 
 void View::displayWinner(int identifier) {
     if(identifier == 0) {
         cout << "It's a tie!" << endl;
+        cout << endl;
     } else if (identifier == 1) {
 //        cout << "
     }
@@ -24,6 +27,8 @@ void View::displayWinner(int identifier) {
 
 void View::displayCardDrawnAndScore(int score, string cardDrawn) {
     cout << "Card drawn: " << cardDrawn << endl;
+    cout << endl;
     cout << "Score: " << score << endl;
+    cout << endl;
 }
 
